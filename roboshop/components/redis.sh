77c7347afb940f123180317fb89900efde2ac007
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 23:36:38 +0530

    Redis component-Updating the configure file

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -10,3 +10,15 @@ Status_Check $?
 Print "Install Redis"
 yum install redis -y &>>LOG_FILE
 Status_Check $?
+
+Print "Update the configure file"
+if [ -f /etc/redis.conf ]; then
+  Print "Updating in /etc/redis.conf file -"
+  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>LOG_FILE
+  Status_Check $?
+fi
+if [ -f /etc/redis/redis.conf ]; then
+  Print "Updating in /etc/redis/redis.conf file -"
+  sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf &>>LOG_FILE
+  Status_Check $?
+fi
