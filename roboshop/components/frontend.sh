97468ce8e5cbf46a851100986a9d8f4ebbe1232e
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 11:08:07 +0530

    Roboshop project - Frontend component

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,1 +1,25 @@
-echo front end component
+#!/bin/bash
+
+echo "Installing Nginx"
+yum install nginx -y
+
+echo "Downloading Nginx"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+
+
+echo "Old files clean up"
+cd /usr/share/nginx/html
+rm -rf *
+
+echo "Unzipping content"
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+
+echo "Configuration set up"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo "Starting Nginx"
+systemctl enable nginx
+systemctl start nginx
