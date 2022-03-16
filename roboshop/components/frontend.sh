a4a14511622ac0e984ff63f08aacfb085a36eb42
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 15:30:36 +0530

    optimizing code for Frontend and Mongodb component

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,32 +1,6 @@
 #!/bin/bash
 
-# checking if the user is a root user or not
-user_id=$(id -u)
-if [ "$user_id" -ne 0 ]
-then
-  echo "You should be a root user to run the script"
-  exit 1
-fi
-
-#function to check if the command execution status is SUCCESS/FAILURE
-Status_Check(){
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mSUCCESS\e[0m"
-  else
-    echo -e "\e[31mFAILURE\e[0m"
-    exit 2
-  fi
-}
-
-#function to print the message comment
-Print()
-{
-  echo -e "\n---------------$1---------------\n" &>>$LOG_FILE
-  echo -e "\e[35m$1\e[0m"
-}
-
-LOG_FILE=/tmp/roboshop.log
-rm -f $LOG_FILE
+source components/common.sh
 
 Print "Installing Nginx"
 yum install nginx -y &>>$LOG_FILE
