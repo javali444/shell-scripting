9e5daa07614565ebd062e1a32c1cdebf0b8fc90a
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-17 00:13:02 +0530

    Creating Cart component for Roboshop project

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -69,7 +69,7 @@ NodeJS(){
   Status_Check $?
 
   Print "Configure SystemD file with correct IP addresses"
-  sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE && sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+  sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE && sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
   Status_Check $?
 
 
