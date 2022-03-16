3039d2253d3aeb4a5577d6972f59cc3d94cdad5d
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 15:21:13 +0530

    Adding Mongodb component to Roboshop project

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -29,17 +29,23 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 Print "Setting up Mongodb repos"
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
-Status_Check &?
+curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
+Status_Check $?
 
 Print "Install Mongodb"
-yum install -y mongodb-org
-Status_Check &?
+yum install -y mongodb-org &>>LOG_FILE
+Status_Check $?
+
+
+Print "Updating Mongodb configuration file"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+Status_Check $?
 
 
 Print "Starting Mongodb"
 systemctl enable mongod &>>LOG_FILE && systemctl start mongod &>>LOG_FILE
-Status_Check &?
+Status_Check $?
+
 
 
 # curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
