34814292af1f7f528d3b22fb5dc5404f9f2d3071
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 18:16:59 +0530

    Creating Catalogue component for Roboshop project

---
+++ roboshop/components/catalogue.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+source components/common.sh
+
+Print "Downloading the Repos"
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>LOG_FILE
+Status_Check $?
+
+Print "Install Node Js"
+yum install nodejs gcc-c++ -y &>>LOG_FILE
+Status_Check $?
+
+Print "Adding Application user"
+useradd $APP_USER &>>LOG_FILE
+Status_Check $?
+
+Print "Downloading Catalogue content"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>LOG_FILE
+Status_Check $?
+
+Print "Clean up old contents"
+rm -rf /home/roboshop/catalogue &>>LOG_FILE
+Status_Check $?
+
+Print "Unzipping the files"
+cd /home/roboshop &>>LOG_FILE && unzip /tmp/catalogue.zip &>>LOG_FILE && mv catalogue-main catalogue &>>LOG_FILE
+Status_Check $?
+
+
+Print "Installing Dependencies"
+cd /home/roboshop/catalogue &>>LOG_FILE && npm install &>>LOG_FILE
+Status_Check $?
--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -27,3 +27,5 @@ Print()
 
 LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
+
+APP_USER=roboshop
