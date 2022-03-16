dc5ab349597fb0415cc2348ecaf4b4475a1d3bdc
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 18:23:12 +0530

    Creating Catalogue component for Roboshop project

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -4,21 +4,21 @@
 source components/common.sh
 
 Print "Setting up Mongodb repos"
-curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
+curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>$LOG_FILE
 Status_Check $?
 
 Print "Install Mongodb"
-yum install -y mongodb-org &>>LOG_FILE
+yum install -y mongodb-org &>>$LOG_FILE
 Status_Check $?
 
 
 Print "Updating Mongodb configuration file"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
 Status_Check $?
 
 
 Print "Starting Mongodb"
-systemctl enable mongod &>>LOG_FILE && systemctl restart mongod &>>LOG_FILE
+systemctl enable mongod &>>LOG_FILE && systemctl restart mongod &>>$LOG_FILE
 Status_Check $?
 
 
