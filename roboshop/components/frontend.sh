9264d4e09bd4cc1ba1b9ed0152d03dc724a3ed58
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 14:19:07 +0530

    Frontend component - adding code to push the logs to a log file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,7 +21,7 @@ Status_Check(){
 #function to print the message comment
 Print()
 {
-  echo -e "\n---------------$1---------------\n" >>$LOG_FILE
+  echo -e "\n---------------$1---------------\n" &>>$LOG_FILE
   echo -e "\e[35m$1\e[0m"
 }
 
@@ -29,31 +29,31 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 Print "Installing Nginx"
-yum install nginx -y >>$LOG_FILE
+yum install nginx -y &>>$LOG_FILE
 Status_Check $?
 
 
 
 Print "Downloading Nginx"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" >>$LOG_FILE
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG_FILE
 Status_Check $?
 
 
 Print "Old files clean up"
-rm -rf /usr/share/nginx/html/* >>$LOG_FILE
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 Status_Check $?
 cd /usr/share/nginx/html
 
 Print "Unzipping content"
-unzip /tmp/frontend.zip >>$LOG_FILE && mv frontend-main/* . >>$LOG_FILE && mv static/* . >>$LOG_FILE && rm -rf frontend-main README.md >>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE && mv frontend-main/* . &>>$LOG_FILE && mv static/* . &>>$LOG_FILE && rm -rf frontend-main README.md &>>$LOG_FILE
 Status_Check $?
 
 
 Print "Configuration set up"
-mv localhost.conf /etc/nginx/default.d/roboshop.conf >>$LOG_FILE
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
 Status_Check $?
 
 
 Print "Starting Nginx"
-systemctl restart nginx && systemctl enable nginx >>$LOG_FILE
+systemctl restart nginx &>>$LOG_FILE && systemctl enable nginx &>>$LOG_FILE
 Status_Check $?
