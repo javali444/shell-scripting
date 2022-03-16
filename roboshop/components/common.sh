e746b8e07941d3d44e080cf84a8574936b9a4958
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-16 23:56:42 +0530

    Creating User component for Roboshop project

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -29,3 +29,52 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 APP_USER=roboshop
+
+NodeJS(){
+  Print "Downloading the Repos"
+  curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>$LOG_FILE
+  Status_Check $?
+
+  Print "Install Node Js"
+  yum install nodejs gcc-c++ -y &>>$LOG_FILE
+  Status_Check $?
+
+  id $APP_USER &>>$LOG_FILE
+  if [ $? -ne 0 ]; then
+    Print "Adding Application user"
+    useradd ${APP_USER} &>>$LOG_FILE
+    Status_Check $?
+  fi
+
+
+  Print "Downloading Catalogue content"
+  curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>$LOG_FILE
+  Status_Check $?
+
+  Print "Clean up old contents"
+  rm -rf /home/${APP_USER}/${COMPONENT} &>>$LOG_FILE
+  Status_Check $?
+
+  Print "Unzipping the files"
+  cd /home/${APP_USER} &>>$LOG_FILE && unzip -o /tmp/${COMPONENT}.zip &>>$LOG_FILE && mv ${COMPONENT}-main ${COMPONENT} &>>$LOG_FILE
+  Status_Check $?
+
+
+  Print "Installing Dependencies"
+  cd /home/${APP_USER}/user &>>$LOG_FILE && npm install &>>$LOG_FILE
+  Status_Check $?
+
+  Print "Applying permissions to Application user"
+  chown -R $APP_USER:$APP_USER /home/$APP_USER
+  Status_Check $?
+
+  Print "Configure SystemD file with correct IP addresses"
+  sed -i -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE && sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>$LOG_FILE && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>$LOG_FILE
+  Status_Check $?
+
+
+  Print "Restart Catalogue service"
+  systemctl daemon-reload &>>$LOG_FILE && systemctl restart catalogue &>>$LOG_FILE && systemctl enable catalogue &>>$LOG_FILE
+  Status_Check $?
+
+}
