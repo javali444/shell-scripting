2433563934c80960024f65dc2c162b090099dc4c
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-18 12:09:25 +0530

    adding security group to ec2 instance

--- create-ec2-instance.sh
+++ create-ec2-instance.sh
@@ -2,4 +2,8 @@
 
 AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t2.micro
+SEC_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g'")
+
+aws ec2 run-instances \
+    --image-id ${AMI_ID} --instance-type t2.micro \
+    --security-group-ids ${SEC_ID}
