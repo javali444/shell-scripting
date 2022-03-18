7023ec4911a83cc097affa20b211b7dd2ac475f3
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-18 12:22:27 +0530

    adding tags to ec2 instance

--- create-ec2-instance.sh
+++ create-ec2-instance.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Input machine name"
+    exit 1
+fi
+
+Component=$1
+
+
 AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 
 SEC_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 aws ec2 run-instances \
     --image-id ${AMI_ID} --instance-type t2.micro \
-    --security-group-ids ${SEC_ID}
+    --security-group-ids ${SEC_ID} \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]"
