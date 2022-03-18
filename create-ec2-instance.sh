f194dce617cceea4f2d8c95491d49252e4e2e3bb
Javali Valluru <javalivallururjsj44@gmail.com>
2022-03-18 22:23:57 +0530

    Creating DNS record

--- create-ec2-instance.sh
+++ create-ec2-instance.sh
@@ -12,9 +12,15 @@ AMI_ID=$( aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pr
 
 SEC_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-from-public" | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-aws ec2 run-instances \
+private_ip=$(aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --instance-type t2.micro \
     --security-group-ids ${SEC_ID} \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${Component}}]" \
-    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
+    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+    | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+
+sed -e "s/COMPONENT/${Component}" -e "s/IPaddress/${private_ip}" route53.json &>> /tmp/DNS_log.json
+
+aws route53 change-resource-record-sets --hosted-zone-id Z051954137QLHFOEJ46UR --change-batch file:///tmp/DNS_log.json | jq
