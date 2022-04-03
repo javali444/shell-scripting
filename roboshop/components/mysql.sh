751c42703dac61a09b7b07160ad8731c92da50aa
Javali Valluru <javalivallururjsj44@gmail.com>
2022-04-03 23:12:03 +0530

    Creating MySQL component

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -22,5 +22,5 @@ if [ 0 -ne $? ]; then
   echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
   DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
   mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
-  StatCheck $?
+  Status_Check $?
 fi
