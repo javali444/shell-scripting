b4b8038928c2893e3a8119e334f8cd3f515f5eb4
Javali Valluru <javalivallururjsj44@gmail.com>
2022-04-03 23:11:27 +0530

    Creating MySQL component

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -16,8 +16,11 @@ systemctl enable mysqld &>>$LOG_FILE && systemctl start mysqld &>>$LOG_FILE
 Status_Check $?
 
 
-Print "Change default password"
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/password.txt
-DEFAULT_PASSWORD= $(grep "temporary password" /var/log/mysqld.log | awk '{print $NF}')
-mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/password.txt &>>${LOG_FILE}
-Status_Check $?
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
+if [ 0 -ne $? ]; then
+  Print "Change Default Root Password"
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+  DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
+  StatCheck $?
+fi
