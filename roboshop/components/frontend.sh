802db3458d8d0525b44e37cb63033312e1325f4c
Javali Valluru <javalivallururjsj44@gmail.com>
2022-05-14 19:05:03 +0530

    Roboshop - Shipping component

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,7 +25,7 @@ Status_Check $?
 
 Print "Configuration set up"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
-for component_value in catalogue user cart; do
+for component_value in catalogue user cart shipping; do
   Print "Updating configuration file of ${component_value} -"
   sed -i -e "/${component_value}/s/localhost/${component_value}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf &>>$LOG_FILE
   Status_Check $?
